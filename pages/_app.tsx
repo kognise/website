0707f598d4a4abb761bf12509c17fd4e4a98a063
Felix Mattick <felix.mattick@gmail.com>
2021-06-04 16:29:56 -0400

    Fix SEO

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import 'styles/theme.css'
 
 import { AppProps } from 'next/app'
 import { DefaultSeo as DefaultSEO } from 'next-seo'
+import { useRouter } from 'next/router'
 import Head from 'next/head'
 import React, { FC, useEffect } from 'react'
 import ReactGA from 'react-ga'
@@ -13,10 +14,12 @@ I'm a young person who's interested in programming, aviation, and music. I'm cur
 `.trim()
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
+	const router = useRouter()
+
 	useEffect(() => {
 		ReactGA.initialize('UA-116663597-5')
-		ReactGA.set({ page: window.location.pathname })
-		ReactGA.pageview(window.location.pathname)
+		ReactGA.set({ page: router.pathname })
+		ReactGA.pageview(router.pathname)
 	}, [])
 
 	return (
@@ -26,44 +29,44 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
 				<link rel='icon' href='https://media.kognise.dev/logos/pfp.png' />
 				<link rel='preconnect' href='https://fonts.gstatic.com' />
 				<link href='https://fonts.googleapis.com/css2?family=Syne:wght@800&display=swap' rel='stylesheet' />
-
-				<DefaultSEO
-					title='Kognise'
-					description={description}
-					canonical='https://kognise.dev/'
-					additionalMetaTags={[
-						{
-							name: 'keywords',
-							content: 'kognise,website,personal,portfolio,projects,about,contact,info,developer,designer,resume',
-						},
-						{
-							name: 'theme-color',
-							content: '#0e7dde',
-						},
-					]}
-					openGraph={{
-						type: 'website',
-						locale: 'en_US',
-						url: 'https://kognise.dev/',
-						title: 'Kognise',
-						description,
-						images: [
-							{
-								url: 'https://branding.kognise.dev/logos/banner.png',
-								width: 1500,
-								height: 500,
-								alt: 'Kognise: Development and Design',
-							},
-						],
-						site_name: 'Kognise',
-					}}
-					twitter={{
-						handle: '@kognise',
-						site: '@kognise',
-						cardType: 'summary_large_image',
-					}}
-				/>
 			</Head>
+
+			<DefaultSEO
+				title='Kognise'
+				description={description}
+				canonical='https://kognise.dev/'
+				additionalMetaTags={[
+					{
+						name: 'keywords',
+						content: 'kognise,website,personal,portfolio,projects,about,contact,info,developer,designer,resume',
+					},
+					{
+						name: 'theme-color',
+						content: '#0e7dde',
+					},
+				]}
+				openGraph={{
+					type: 'website',
+					locale: 'en_US',
+					url: 'https://kognise.dev/',
+					title: 'Kognise',
+					description,
+					images: [
+						{
+							url: 'https://branding.kognise.dev/logos/banner.png',
+							width: 1500,
+							height: 500,
+							alt: 'Kognise: Development and Design',
+						},
+					],
+					site_name: 'Kognise',
+				}}
+				twitter={{
+					handle: '@kognise',
+					site: '@kognise',
+					cardType: 'summary_large_image',
+				}}
+			/>
 
 			<Component {...pageProps} />
 		</>
